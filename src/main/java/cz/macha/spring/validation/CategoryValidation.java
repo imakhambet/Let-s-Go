43bf4a1643f72a7c56f6e0bb29dab051a5aec100
Makhambet Ismukhambetov <ismukmak@fel.cvut.cz>
2020-01-05 12:45:56 +0100

    validation

---
+++ src/main/java/cz/macha/spring/validation/CategoryValidation.java
@@ -0,0 +1,38 @@
+package cz.macha.spring.validation;
+
+import cz.macha.spring.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class CategoryValidation {
+
+    private final CategoryService categoryService;
+    private List<String> errors = new ArrayList<>();
+
+
+    @Autowired
+    public CategoryValidation(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    public void validation(String name){
+        name(name);
+    }
+
+    private void name(String name){
+        if(name.isEmpty()){
+            errors.add("Category is required");
+        }
+        else if(categoryService.getCategoryByName(name) != null){
+            errors.add("Category exists");
+        }
+    }
+
+    public List<String> getErrors() {
+        return errors;
+    }
+}
